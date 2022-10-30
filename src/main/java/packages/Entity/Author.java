03b7b0c81b19147feb9be1fc3c34d1dd396dfdc9
rocketpowers <rocketpowerswillian@gmail.com>
2022-10-30 11:03:20 -0300

    author entity working

---
+++ src/main/java/packages/Entity/Author.java
@@ -0,0 +1,32 @@
+package packages.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Table(name = "author")
+@Entity(name = "author")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Author {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	public Author(String name, Long id) {
+		this.name = name;
+		this.id = id;
+	}
+
+}
